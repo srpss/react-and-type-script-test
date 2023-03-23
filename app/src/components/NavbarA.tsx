2e427ab5ea1b1adeb49c5ddc25d789d2a9d9c4ce
srpss <61586478+srpss@users.noreply.github.com>
2023-03-23 18:51:02 +0200

    navbar complete

---
+++ app/src/components/NavbarA.tsx
@@ -0,0 +1,31 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
+import { NavLink } from "react-router-dom"
+export function Navbar() {
+
+    return <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+        <Container>
+            <Nav className="me-auto">
+                <Nav.Link to="/" as={NavLink}>
+                    Home
+                </Nav.Link>
+                <Nav.Link to="/store" as={NavLink}>
+                    Store
+                </Nav.Link>
+                <Nav.Link to="/about" as={NavLink}>
+                    About
+                </Nav.Link>
+            </Nav>
+            <Button style={{ width: "3rem", height: "3rem", position: "relative" }}
+                variant="outline-primary"
+                className="rounded-circle">
+
+                <svg
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                </svg>
+                <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{color:"white", width: "1.5rem", height:"1.5rem", position:"absolute", bottom:0, right:0, transform: "translate(25%,25%)"}}>
+                    3
+                </div>
+            </Button>
+        </Container>
+    </NavbarBs>
+}
